9dcc66d0aaf5020caed2d61bfe8155f1f74b1ddb
Alexander Weber <web.alexander@web.de>
2017-01-17 11:35:53 +0100

    Enhance search_usable_destination stuck detection slow jump if in catched in walkable node

--- npcf/control.lua
+++ npcf/control.lua
@@ -22,7 +22,8 @@ local control_framework = {
 			control.walk_param = {
 				find_path = true,
 				direct_way = true,
-				max_find_path_distance = 25
+				max_find_path_distance = 25,
+				search_usable_destination = true
 			}
 		end
 		if control._step_init_done ~= true then
@@ -38,6 +39,7 @@ local control_framework = {
 			control._speed_bak = control.speed
 			control._dest_bak = control.path[1]
 			control._step_init_done = true
+			control.real_speed = math.sqrt( math.pow(control.velocity.x,2) + math.pow(control.velocity.z,2))
 		end
 		return control
 	end
@@ -88,31 +90,54 @@ end
 -- start walking to pos
 function control_proto:walk(pos, speed)
 	self.speed = speed
+	self.path  = nil
 	if self.walk_param.find_path == true and vector.distance(self.pos, pos) < self.walk_param.max_find_path_distance then
 		local startpos = vector.round(self.pos)
 		startpos.y = startpos.y - 1
 
-		local destpos = table.copy(pos)
-		-- use the high of the first non-walkable node (air)
-		for i = pos.y + 1, pos.y - 1, -1 do
-			destpos.y = i
-			local node = minetest.get_node(destpos)
-			local nodedef = minetest.registered_nodes[node.name]
-			if not (node.name == "air" or nodedef and (nodedef.walkable == false or nodedef.drawtype == "airlike")) then
-				destpos.y = destpos.y +1
-				break
+		local destpos
+		if self.search_usable_destination then
+			local rpos = vector.round(pos)
+			for y = rpos.y+2, rpos.y-2, -1 do
+				for x = rpos.x-2, rpos.x+2 do
+					for z = rpos.z-2, rpos.z+2 do
+						local p = {x=x, y=y, z=z}
+						local node = minetest.get_node(p)
+						local nodedef = minetest.registered_nodes[node.name]
+						if not (node.name == "air" or nodedef and (nodedef.walkable == false or nodedef.drawtype == "airlike")) then
+							p.y = p.y +1
+							if destpos == nil or vector.distance(p, pos) < vector.distance(destpos, pos) then
+								destpos = p
+							end
+						end
+					end
+				end
+				if destpos ~= nil then
+					break
+				end
 			end
+		end
+		if not destpos then
+			destpos = self.pos
+		else
+			print("selected:", minetest.pos_to_string(destpos), "for", minetest.pos_to_string(self.pos))
 		end
 		local path = minetest.find_path(startpos, destpos, 10, 1, 5, "Dijkstra")
 		self.path = path
-		if not self.path or not self.path[1] and self.walk_param.direct_way == true then
+		if not self.path and self.walk_param.direct_way == true then
 			self.path = { pos }
+			print("fallback path")
+		elseif self.path then
+			print("calculated path")
+			table.insert(self.path, pos)
 		end
-	elseif self.walk_param.direct_way == true then
-		self.path = { pos }
 	end
-	if not self.path then
+
+	if self.walk_param.direct_way == true and self.path == nil then
+		self.path = { pos }
+	elseif self.path == nil then
 		self:stay()
+		self:look_to(pos)
 	end
 end
 
@@ -127,6 +152,12 @@ function control_proto:_do_control_step(dtime)
 	self._step_timer = 0
 	control_framework.getControl(self._npc)
 	self._step_init_done = false
+
+
+	if self._speed_bak > 0 and self.real_speed < 0.1 then
+		print("Stuck!")
+		self:stay()
+	end
 
 	-- check path
 	if self.speed > 0 then
@@ -151,7 +182,7 @@ function control_proto:_do_control_step(dtime)
 		end
 	end
 	-- check/set yaw
-	if self.path[1] then
+	if self.path[1] and self.speed > 0 then
 		self.yaw = npcf:get_face_direction(self.pos, self.path[1])
 	end
 	if self.yaw  ~= self._yaw_bak then
@@ -178,7 +209,7 @@ function control_proto:_do_control_step(dtime)
 		npcf:set_animation(self._npc, self._state)
 	end
 
-	-- check for water
+	-- check for current environment
 	local nodepos = table.copy(self.pos)
 	local node = {}
 	nodepos.y = nodepos.y - 0.5
@@ -199,8 +230,11 @@ function control_proto:_do_control_step(dtime)
 	elseif minetest.find_node_near(self.pos, 2, {"group:water"}) then
 		-- Light-footed near water
 		self.acceleration = {x=0, y=-1, z=0}
+	elseif minetest.registered_nodes[node[-1].name].walkable ~= false then
+		-- jump if in catched in walkable node
+		self.velocity.y = 1
 	else
-		-- walking - check for jump
+		-- walking
 		self.acceleration = {x=0, y=-10, z=0}
 	end
 	if self.acceleration.y ~= self._acceleration_bak.y then
